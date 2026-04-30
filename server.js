099d148e1b45592f7b24f15f8480584eddc7588e
yuuura127 <yagajanian@gmail.com>
2026-04-30 22:45:34 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const io = new Server(process.env.PORT || 3000, { cors: { origin: "*" } });
 
 let players = {};
 const TILE_SIZE = 60;
+const PLAYER_SPEED = 2.5; // Замедлил персонажей
 
-// Карта точно как на скриншоте image_d5d65a.png, заполняющая все пространство
+// Огромная карта на весь экран (32x18 блоков)
 const map = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,1,1,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
 io.on("connection", (socket) => {
@@ -27,10 +32,9 @@ io.on("connection", (socket) => {
     socket.on("move", (move) => {
         let p = players[socket.id];
         if (p && !p.dead) {
-            let nextX = p.x + move.x;
-            let nextY = p.y + move.y;
+            let nextX = p.x + (move.x > 0 ? PLAYER_SPEED : move.x < 0 ? -PLAYER_SPEED : 0);
+            let nextY = p.y + (move.y > 0 ? PLAYER_SPEED : move.y < 0 ? -PLAYER_SPEED : 0);
             
-            // Проверка коллизий (хитбокс 15px)
             const isWall = (nx, ny) => {
                 let mx = Math.floor(nx / TILE_SIZE);
                 let my = Math.floor(ny / TILE_SIZE);
@@ -49,8 +53,23 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on("playerHit", (id) => { if(players[id]) { players[id].dead = true; io.to(id).emit("youDied"); io.emit("updatePlayers", players); }});
-    socket.on("killConfirmed", (id) => { if(players[id]) { players[id].score++; io.emit("updatePlayers", players); }});
-    socket.on("respawn", () => { if(players[socket.id]) { players[socket.id].x = 120; players[socket.id].y = 120; players[socket.id].dead = false; io.emit("updatePlayers", players); }});
+    // Важно: регистрация смерти теперь более строгая
+    socket.on("requestKill", (victimId) => {
+        if (players[victimId] && !players[victimId].dead) {
+            players[victimId].dead = true;
+            io.to(victimId).emit("youDied");
+            if (players[socket.id]) players[socket.id].score++;
+            io.emit("updatePlayers", players);
+        }
+    });
+
+    socket.on("respawn", () => {
+        if (players[socket.id]) {
+            players[socket.id].x = 120; players[socket.id].y = 120;
+            players[socket.id].dead = false;
+            io.emit("updatePlayers", players);
+        }
+    });
+
     socket.on("disconnect", () => { delete players[socket.id]; io.emit("updatePlayers", players); });
 });
