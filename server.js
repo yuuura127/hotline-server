c38c2c056617de0d71f5505228d5051733ee284a
yuuura127 <yagajanian@gmail.com>
2026-04-30 21:56:54 +0400

    Refactor server.js to include HTML game interface
    Removed socket.io server setup and added HTML structure for the game interface.

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
-const io = require('socket.io')(process.env.PORT || 3000, {
-    cors: { origin: "*" } 
-});
-
-let players = {};
-
-io.on('connection', (socket) => {
-    console.log('Игрок подключился:', socket.id);
-    
-    // Создаем нового игрока со случайными координатами
-    players[socket.id] = { 
-        x: Math.random() * 700 + 50, 
-        y: Math.random() * 500 + 50, 
-        color: Math.random() > 0.5 ? '#00ffff' : '#ff00ff' 
-    };
-
-    // Отправляем всем текущее состояние игры
-    io.emit('updatePlayers', players);
-
-    // Обработка движения
-    socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x += data.x;
-            players[socket.id].y += data.y;
-            io.emit('updatePlayers', players);
-        }
-    });
-
-    // Обработка отключения
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('updatePlayers', players);
-    });
-});
-
-console.log('Сервер запущен!');
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+    <meta charset="UTF-8">
+    <title>Hotline Online 1vs1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="game-container">
+        <div class="stats">
+            <div id="p1-score">თქვენ: 0</div>
+            <h1 class="game-title">HOTLINE ONLINE</h1>
+            <div id="p2-score">მტერი: 0</div>
+        </div>
+        <canvas id="gameCanvas" width="800" height="480"></canvas>
+        <div id="status">დაკავშირება...</div>
+    </div>
+    <script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
