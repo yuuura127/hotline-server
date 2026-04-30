3a92745104a6babb92fa0fea24b5eb02c589a271
yuuura127 <yagajanian@gmail.com>
2026-04-30 22:41:18 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,23 @@ const io = new Server(process.env.PORT || 3000, { cors: { origin: "*" } });
 let players = {};
 const TILE_SIZE = 60;
 
-// Карта на основе image_c8c634.png
+// Карта точно как на скриншоте image_d5d65a.png, заполняющая все пространство
 const map = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,0,1,1,1,1,0,0,0,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1],
-    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1],
+    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
 io.on("connection", (socket) => {
     socket.on("joinGame", (nickname) => {
-        players[socket.id] = { x: 300, y: 300, nickname: nickname || "Player", score: 0, dead: false };
+        players[socket.id] = { x: 120, y: 120, nickname: nickname || "Player", score: 0, dead: false };
         io.emit("updatePlayers", players);
     });
 
@@ -31,13 +29,16 @@ io.on("connection", (socket) => {
         if (p && !p.dead) {
             let nextX = p.x + move.x;
             let nextY = p.y + move.y;
-            const checkWall = (nx, ny) => {
+            
+            // Проверка коллизий (хитбокс 15px)
+            const isWall = (nx, ny) => {
                 let mx = Math.floor(nx / TILE_SIZE);
                 let my = Math.floor(ny / TILE_SIZE);
                 return map[my] && map[my][mx] === 1;
             };
-            if (!checkWall(nextX, p.y)) p.x = nextX;
-            if (!checkWall(p.x, nextY)) p.y = nextY;
+
+            if (!isWall(nextX, p.y)) p.x = nextX;
+            if (!isWall(p.x, nextY)) p.y = nextY;
             io.emit("updatePlayers", players);
         }
     });
@@ -48,28 +49,8 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on("playerHit", (victimId) => {
-        if (players[victimId] && !players[victimId].dead) {
-            players[victimId].dead = true;
-            io.to(victimId).emit("youDied");
-            io.emit("updatePlayers", players);
-        }
-    });
-
-    socket.on("killConfirmed", (killerId) => {
-        if (players[killerId]) {
-            players[killerId].score += 1;
-            io.emit("updatePlayers", players);
-        }
-    });
-
-    socket.on("respawn", () => {
-        if (players[socket.id]) {
-            players[socket.id].x = 300; players[socket.id].y = 300;
-            players[socket.id].dead = false;
-            io.emit("updatePlayers", players);
-        }
-    });
-
+    socket.on("playerHit", (id) => { if(players[id]) { players[id].dead = true; io.to(id).emit("youDied"); io.emit("updatePlayers", players); }});
+    socket.on("killConfirmed", (id) => { if(players[id]) { players[id].score++; io.emit("updatePlayers", players); }});
+    socket.on("respawn", () => { if(players[socket.id]) { players[socket.id].x = 120; players[socket.id].y = 120; players[socket.id].dead = false; io.emit("updatePlayers", players); }});
     socket.on("disconnect", () => { delete players[socket.id]; io.emit("updatePlayers", players); });
 });
